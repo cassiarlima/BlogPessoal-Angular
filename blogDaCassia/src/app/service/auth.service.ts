78015b976548f2d3b44693d8f9c4720b0d3b94a9
Cássia Lima <cassiadosreislima@gmail.com>
2021-11-06 16:24:30 -0300

    Componente inicio sem consumo

--- blogDaCassia/src/app/service/auth.service.ts
+++ blogDaCassia/src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
+import { environment } from './../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,15 @@ export class AuthService {
 
   cadastrar(user: User): Observable<User> {
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+  }
+
+  logado(){
+    let ok = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+    
+    return ok
   }
 }

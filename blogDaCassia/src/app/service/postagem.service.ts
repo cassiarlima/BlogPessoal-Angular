ba3a799643dd611967fa4542ede50e0596cff4c6
Cássia Lima <cassiadosreislima@gmail.com>
2021-11-13 00:43:42 -0300

    Implementação do post e get de postagens

--- blogDaCassia/src/app/service/postagem.service.ts
+++ blogDaCassia/src/app/service/postagem.service.ts
@@ -15,9 +15,10 @@ export class PostagemService {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
-  getAllPostagens(): Observable<Postagem[]>{
-    return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
+  getAllPostagens(): Observable<Postagem[]> {
+    return this.http.get<Postagem[]>(`http://localhost:8080/postagens/`, this.token)
   }
+
 
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
